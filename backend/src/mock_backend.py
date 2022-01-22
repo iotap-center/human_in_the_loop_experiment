79d10a17969f1833f1f2c5ee3bbbf81fd1d8e518
Johan Holmberg <johan@idioti.se>
2022-01-22 22:37:57 +0100

    Added mock objects used during development

--- backend/src/mock_backend.py
+++ backend/src/mock_backend.py
@@ -47,24 +47,19 @@ def create_session():
             'steps': {
                 'href': base_url + '/sessions/' + str(session) + '/steps',
                 'method': 'GET'
-            },
-            'first': {
-                'href': base_url + '/sessions/' + str(session) + '/steps/1',
-                'method': 'GET'
             }
         }
     }
     return jsonify(data)
 
-def get_steps(session: uuid):
+def list_steps(session: uuid):
     data = {
         'session': str(session),
         'steps': [
             base_url + '/sessions/' + str(session) + '/steps/1',
             base_url + '/sessions/' + str(session) + '/steps/2',
             base_url + '/sessions/' + str(session) + '/steps/3',
-            base_url + '/sessions/' + str(session) + '/steps/4',
-            base_url + '/sessions/' + str(session) + '/steps/5'
+            base_url + '/sessions/' + str(session) + '/steps/4'
         ],
         'links': {
             'self': {
@@ -72,11 +67,11 @@ def get_steps(session: uuid):
                 'method': 'GET'
             },
             'first': {
-                'href': base_url + '/sessions/' + str(session) + 'steps/1',
+                'href': base_url + '/sessions/' + str(session) + '/steps/1',
                 'method': 'GET'
             },
             'last': {
-                'href': base_url + '/sessions/' + str(session) + 'steps/5',
+                'href': base_url + '/sessions/' + str(session) + '/steps/4',
                 'method': 'GET'
             }
         }
@@ -87,42 +82,22 @@ def get_step(session: uuid, step: int):
     data = {
         'session': str(session),
         'step': 1,
-        'images': [
+        'subsessions': [
             {
-                'subsession': 1,
-                'image': 'dummy.cat.jpg',
-                'labels': ['katt', 'hund'],
-                'prediction': 0,
-                'classification': -1,
-                'image_url': '/images/dummy.cat.jpg'
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/1',
+                'method': 'GET'
             },
             {
-                'subsession': 2,
-                'image': 'dummy.ball.jpg',
-                'labels': ['puck', 'boll'],
-                'prediction': 1,
-                'classification': -1,
-                'image_url': '/images/dummy.ball.png'
-            },
-            {
-                'subsession': 3,
-                'image': 'dummy.apple.jpg',
-                'labels': ['äpple', 'banan'],
-                'prediction': 0,
-                'classification': -1,
-                'image_url': '/images/dummy.apple.jpg'
-            },
-            {
-                'subsession': 4,
-                'image': 'dummy.bat.jpg',
-                'labels': ['fladdermus', 'tjuv'],
-                'prediction': 1,
-                'classification': -1,
-                'image_url': '/images/dummy.bat.jpg'
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/2',
+                'method': 'GET'
             }
         ],
         'links': {
             'self': {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step),
+                'method': 'GET'
+            },
+            'first': {
                 'href': base_url + '/sessions/' + str(session) + '/steps/1',
                 'method': 'GET'
             },
@@ -130,23 +105,146 @@ def get_step(session: uuid, step: int):
                 'href': base_url + '/sessions/' + str(session) + '/steps/2',
                 'method': 'GET'
             },
+            'last': {
+                'href': base_url + '/sessions/' + str(session) + '/steps/4',
+                'method': 'GET'
+            },
+            'parent': {
+                'href': base_url + '/sessions/' + str(session),
+                'method': 'GET'
+            }
+        }
+    }
+    return jsonify(data)
+
+def list_subsession_steps(session: uuid, step: int, subsession: int):
+    data = {
+        'session': str(session),
+        'step': 1,
+        'subsession': subsession,
+        'subsession_steps': [
+            {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession) + '/steps/1',
+                'method': 'GET'
+            },
+            {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession) + '/steps/2',
+                'method': 'GET'
+            },
+            {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession) + '/steps/3',
+                'method': 'GET'
+            },
+            {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession) + '/steps/4',
+                'method': 'GET'
+            },
+            {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession) + '/steps/5',
+                'method': 'GET'
+            }
+        ],
+        'links': {
+            'self': {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession),
+                'method': 'GET'
+            },
+            'first': {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/1',
+                'method': 'GET'
+            },
+            'next': {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/2',
+                'method': 'GET'
+            },
+            'last': {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/2',
+                'method': 'GET'
+            },
+            'parent': {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step),
+                'method': 'GET'
+            }
+        }
+    }
+    return jsonify(data)
+
+def get_subsession_step(session: uuid, step: int, subsession: int, substep: int):
+    data = {
+        'session': str(session),
+        'step': 1,
+        'subsession': subsession,
+        'subsession_step': substep,
+        'images': [
+            {
+                'stream': 1,
+                'image': 'dummy.cat.jpg',
+                'labels': ['katt', 'hund'],
+                'prediction': 0,
+                'classification': -1,
+                'image_url': '/images/dummy.cat.jpg'
+            },
+            {
+                'stream': 2,
+                'image': 'dummy.ball.jpg',
+                'labels': ['puck', 'boll'],
+                'prediction': 1,
+                'classification': -1,
+                'image_url': '/images/dummy.ball.png'
+            },
+            {
+                'stream': 3,
+                'image': 'dummy.apple.jpg',
+                'labels': ['äpple', 'banan'],
+                'prediction': 0,
+                'classification': -1,
+                'image_url': '/images/dummy.apple.jpg'
+            },
+            {
+                'stream': 4,
+                'image': 'dummy.bat.jpg',
+                'labels': ['fladdermus', 'tjuv'],
+                'prediction': 1,
+                'classification': -1,
+                'image_url': '/images/dummy.bat.jpg'
+            }
+        ],
+        'timeout': 5,
+        'links': {
+            'self': {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession) + '/steps/1',
+                'method': 'GET'
+            },
+            'next': {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession) + '/steps/2',
+                'method': 'GET'
+            },
+            'last': {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession) + '/steps/5',
+                'method': 'GET'
+            },
+            'parent': {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession),
+                'method': 'GET'
+            },
             'update': {
-                'href': base_url + '/sessions/' + str(session) + '/steps/1',
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession) + '/steps/1',
                 'method': 'PUT'
             }
         }
     }
     return jsonify(data)
 
-def update_step(session: uuid, step: int):
+def update_subsession_step(session: uuid, step: int):
     images = request.get_json()['images']
 
     data = {
         'session': str(session),
         'step': 1,
+        'subsession': '1.1',
         'images': [
             {
-                'subsession': 1,
+                'stream': 1,
                 'image': 'dummy.cat.jpg',
                 'labels': ['katt', 'hund'],
                 'prediction': 0,
@@ -154,7 +252,7 @@ def update_step(session: uuid, step: int):
                 'image_url': '/images/dummy.cat.jpg'
             },
             {
-                'subsession': 2,
+                'stream': 2,
                 'image': 'dummy.ball.jpg',
                 'labels': ['puck', 'boll'],
                 'prediction': 1,
@@ -162,7 +260,7 @@ def update_step(session: uuid, step: int):
                 'image_url': '/images/dummy.ball.png'
             },
             {
-                'subsession': 3,
+                'stream': 3,
                 'image': 'dummy.apple.jpg',
                 'labels': ['äpple', 'banan'],
                 'prediction': 0,
@@ -170,7 +268,7 @@ def update_step(session: uuid, step: int):
                 'image_url': '/images/dummy.apple.jpg'
             },
             {
-                'subsession': 4,
+                'stream': 4,
                 'image': 'dummy.bat.jpg',
                 'labels': ['fladdermus', 'tjuv'],
                 'prediction': 1,
@@ -180,15 +278,23 @@ def update_step(session: uuid, step: int):
         ],
         'links': {
             'self': {
-                'href': base_url + '/sessions/' + str(session) + '/steps/1',
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession) + '/steps/1',
                 'method': 'GET'
             },
             'next': {
-                'href': base_url + '/sessions/' + str(session) + '/steps/2',
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession) + '/steps/2',
+                'method': 'GET'
+            },
+            'next': {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession) + '/steps/5',
+                'method': 'GET'
+            },
+            'parent': {
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession),
                 'method': 'GET'
             },
             'update': {
-                'href': base_url + '/sessions/' + str(session) + '/steps/1',
+                'href': base_url + '/sessions/' + str(session) + '/steps/' + str(step) + '/subsessions/' + str(subsession) + '/steps/1',
                 'method': 'PUT'
             }
         }
---
+++ backend/src/mock_storage.py
@@ -0,0 +1,51 @@
+from session import Session
+import uuid
+
+class Storage:
+
+    def __init__(self) -> None:
+        self.__store: dict = dict()
+
+    def add_session(self, session: Session) -> None:
+        self.__store[session.get_id().int] = session
+
+    def list_sessions(self) -> list:
+        sessions = list()
+        
+        for key in self.__store:
+            sessions.append(uuid.UUID(int=key))
+
+        return sessions
+
+    def get_sessions(self) -> dict:
+        return self.__store.copy()
+
+    def get_session(self, session: uuid) -> Session:
+        if session.int in self.__store:
+            return self.__store[session.int]
+        else:
+            return None
+
+    def get_nbr_of_steps(self, session: uuid) -> int:
+        steps: int = -1
+        
+        if session.int in self.__store:
+            steps = self.__store[session.int].nbr_of_steps()
+
+        return steps
+
+    def get_steps(self, session: uuid) -> list:
+        steps: list = None
+        
+        if session.int in self.__store:
+            steps = self.__store[session.int].get_steps()
+
+        return steps
+
+    def get_step(self, session: uuid, step_id: int) -> dict:
+        step: dict = None
+        
+        if session.int in self.__store:
+            step = self.__store[session.int]['steps'][step_id]
+
+        return step
